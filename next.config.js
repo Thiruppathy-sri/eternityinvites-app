979ede3192abd7251c3a3e23f1bab5fe0a383f57
Thiruppathy-sri <thiruppathy.sri@gmail.com>
2024-02-22 21:17:05 +0530

    nexxt-config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: "export",
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      // Add other routes as needed
+    };
+  },
 };
-
-module.exports = nextConfig;
